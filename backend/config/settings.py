3fb038a4ba729e50c9ae59488e7d5096fa9c4bce
Danzan Tankhaev <dtankhaev@gmail.com>
2023-06-18 13:53:49 -0400

    COMPLETE

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -19,22 +19,32 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
     "rest_framework",
     "rest_framework_simplejwt",
+
     "api.apps.ApiConfig",
     "items.apps.ItemsConfig",
     "users.apps.UsersConfig",
+
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+
+    "corsheaders.middleware.CorsMiddleware",
+
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_URLS_REGEX = r'^/api/.*$'
 
 ROOT_URLCONF = "config.urls"
 
