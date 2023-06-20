e1f6ef7677397899900f1d580427441af6fa8329
Danzan Tankhaev <dtankhaev@gmail.com>
2023-06-20 00:59:12 -0400

    debug=False

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -10,9 +10,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('TOKEN', default='key')
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
